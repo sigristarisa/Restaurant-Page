baa0c656a6e29c7a03f5cf9663d785f14d632a24
sigristarisa <arisasigrist.ch@gmail.com>
2021-09-28 13:15:47 +0200

    initial commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,34 +1,64 @@
 const Header = () => {
+  //header //
   const header = document.createElement("div");
-  const title = document.createElement("h1");
+  header.setAttribute("id", "header");
 
-  header.appendChild(title);
-  title.innerHTML = "World Piece";
+  // logo //
+  const logo = document.createElement("img");
+  logo.src = "../../assets/pizza-dark-orange.svg";
+  logo.setAttribute("id", "logo");
 
+  // append the logo and the navibar to the header //
+  header.appendChild(logo);
   header.appendChild(NavBar());
 
   return header;
 };
 
+// create the navibar that is in the header //
 const NavBar = () => {
+  // navi bar //
   const navBar = document.createElement("ul");
   navBar.setAttribute("id", "navbar");
 
+  // buttons in the navi //
   const home = document.createElement("li");
   const menu = document.createElement("li");
   const contact = document.createElement("li");
-
-  navBar.appendChild(home);
-  navBar.appendChild(menu);
-  navBar.appendChild(contact);
 
   home.innerHTML = "<button>Home</button>";
   menu.innerHTML = "<button>Menu</button>";
   contact.innerHTML = "<button>Contact</button>";
 
+  // append the buttons to the navi bar //
+  navBar.appendChild(home);
+  navBar.appendChild(menu);
+  navBar.appendChild(contact);
+
   return navBar;
 };
 
+const Body = () => {
+  // body //
+  const body = document.createElement("div");
+  body.setAttribute("id", "body");
+
+  // main image //
+  const mainImage = document.createElement("img");
+  mainImage.src = "../../assets/main-image-edited.png";
+  mainImage.setAttribute("id", "mainImage");
+
+  // restaurant name //
+  const title = document.createElement("h1");
+  title.innerText = "World Piece";
+
+  body.appendChild(mainImage);
+  body.appendChild(title);
+
+  return body;
+};
+
+// create a footer //
 const Footer = () => {
   const footer = document.createElement("footer");
   const copyright = document.createElement("p");
@@ -40,17 +70,10 @@ const Footer = () => {
   return footer;
 };
 
-// const Body = () => {
-//   const image = document.createElement("img");
-//   image.src = "../../assets/main-image.jpg";
-
-//   return image;
-// };
-
 const mainPage = () => {
   const content = document.getElementById("content");
   content.appendChild(Header());
-  //   content.appendChild(Body());
+  content.appendChild(Body());
   content.appendChild(Footer());
 };
 
